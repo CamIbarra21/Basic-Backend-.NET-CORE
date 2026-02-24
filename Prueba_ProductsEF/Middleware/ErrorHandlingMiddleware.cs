5e6e3cd0a0dcca23425f737d182f9729278b5e0d
Camila Ibarra <cami05aic@gmail.com>
2026-02-24 15:15:36 -0500

    feature: add error handling middleware class

---
+++ Prueba_ProductsEF/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using Prueba_ProductsEF.Models;
+
+namespace Prueba_ProductsEF.Middleware
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ErrorHandlingMiddleware> _logger;
+        public ErrorHandlingMiddleware(RequestDelegate next, ILogger<ErrorHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvoceAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Unexpected error in the app");
+
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+                var response = new APIResponse(false, ex.Message);
+
+                await context.Response.WriteAsJsonAsync(response);
+            }
+        }
+    }
+}
