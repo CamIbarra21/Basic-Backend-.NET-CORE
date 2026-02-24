abad1d216d1a3d54f0038fd4f000f847d9ecd552
Camila Ibarra <cami05aic@gmail.com>
2026-02-24 15:47:08 -0500

    feat: add request logs middleware

---
+++ Prueba_ProductsEF/Middleware/RequestLogsMiddleware.cs
@@ -0,0 +1,25 @@
+﻿namespace Prueba_ProductsEF.Middleware
+{
+    public class RequestLogsMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<RequestLogsMiddleware> _logger;
+
+        public RequestLogsMiddleware(RequestDelegate next, ILogger<RequestLogsMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            //_logger.LogInformation("Request: {Method} {Path}", context.Request.Method, context.Request.Path);
+            Console.WriteLine($"Request: {context.Request.Method} in {context.Request.Path}");
+
+            await _next(context);
+
+            //_logger.LogInformation("Response: {StatusCode}", context.Response.StatusCode);
+            Console.WriteLine($"Response: {context.Response.StatusCode}");
+        }
+    }
+}
